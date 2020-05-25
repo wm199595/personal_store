1d37fa63d634ebd4e2d3f4174348d1e8f60c35b4
wm199515 <Administrator@BF-201905181226>
2020-05-25 15:19:10 +0800

    IU-2020.1 <Administrator@BF-201905181226 Create markdown.xml, ide.general.xml, git.xml, project.default.xml, Enum.java, Interface.java, Default.xml, ui.lnf.xml, File Header.java, filetypes.xml, colors.scheme.xml, github.xml, mavenVersion.xml, templates.xml, laf.xml, docker.xml, Class.java, jdk.table.xml, path.macros.xml, wsl.distributions.xml, proxy.settings.xml, vcs.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,5 @@
+
+/**
+ * 
+ * @date ${YEAR}-${MONTH}-${DAY} ${TIME}  
+ */
---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,7 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* Author Peng
+*/
+public interface ${NAME} {
+}
